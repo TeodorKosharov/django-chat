777d35248a44662fcc66af5dc12e5c4a07d122e2
TeodorKosharov <teodor.kosharov@gmail.com>
2023-01-24 04:27:05 +0200

    Configurated main urls and fixed staticfiles dir

--- django_chat/settings.py
+++ django_chat/settings.py
@@ -86,7 +86,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (
-        BASE_DIR / 'staticfiles'
+        BASE_DIR / 'staticfiles',
 )
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- django_chat/urls.py
+++ django_chat/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('/', include('django_chat.core.urls')),
+    path('account/', include('django_chat.account.urls'))
 ]
