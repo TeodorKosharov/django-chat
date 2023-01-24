84a0faf6001639b74428798b91d352c05f6a85e9
TeodorKosharov <teodor.kosharov@gmail.com>
2023-01-24 04:38:17 +0200

    Added home view and prepare to upload to AWS

--- django_chat/core/urls.py
+++ django_chat/core/urls.py
@@ -1,3 +1,7 @@
+from django.urls import path
+from django_chat.core.views import home
+
 urlpatterns = (
+    path('', home),
 
 )
--- django_chat/urls.py
+++ django_chat/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('django_chat.core.urls')),
+    path('', include('django_chat.core.urls')),
     path('account/', include('django_chat.account.urls'))
 ]
