73235dca7132812553263185872bfc6acf76a784
TeodorKosharov <teodor.kosharov@gmail.com>
2023-02-02 21:09:38 +0200

    Added ChatRoom and Message models

--- django_chat/core/models.py
+++ django_chat/core/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+UserModel = get_user_model()
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(UserModel, on_delete=models.CASCADE, unique=False)
+    message = models.CharField(max_length=300, null=False, blank=False)
+
+
+class ChatRoom(models.Model):
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, unique=False)
