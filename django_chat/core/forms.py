89b5ecd5d5a33f9ee5be07f18510e215ff5bb633
TeodorKosharov <teodor.kosharov@gmail.com>
2023-02-07 02:26:51 +0200

    Build responsive design for home page v2

--- django_chat/core/forms.py
+++ django_chat/core/forms.py
@@ -6,9 +6,27 @@ class AddChatRoomForm(forms.ModelForm):
     class Meta:
         model = ChatRoom
         fields = '__all__'
+        labels = {
+            'room_name': ''
+        }
+        widgets = {
+            'room_name': forms.TextInput(attrs={
+                'placeholder': 'Create chat room',
+                'class': 'room-form-input'
+            })
+        }
 
 
 class EnterChatRoomForm(forms.ModelForm):
     class Meta:
         model = ChatRoom
         fields = '__all__'
+        labels = {
+            'room_name': ''
+        }
+        widgets = {
+            'room_name': forms.TextInput(attrs={
+                'placeholder': 'Enter chat room',
+                'class': 'room-form-input'
+            })
+        }
