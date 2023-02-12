d4787b9ed7be67fe1521a1b39920bacabddcd876
TeodorKosharov <teodor.kosharov@gmail.com>
2023-02-12 02:22:12 +0200

    Modify register form and styled register page

--- django_chat/account/forms.py
+++ django_chat/account/forms.py
@@ -11,9 +11,22 @@ class RegisterForm(UserCreationForm):
         model = UserModel
         fields = ('username', 'password1', 'password2')
         field_classes = {'username': UsernameField}
+        labels = {
+            'username': '',
+            'password1': '',
+            'password2': ''
+        }
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': 'Username'}),
+        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.fields['password1'].widget.attrs.update({'placeholder': 'Password'})
+        self.fields['password1'].label = ''
+
+        self.fields['password2'].widget.attrs.update({'placeholder': 'Confirm password'})
+        self.fields['password2'].label = ''
 
         for field in self.fields:
             self.fields[field].help_text = None
@@ -37,17 +50,3 @@ class LoginForm(AuthenticationForm):
             }
         )
     )
-
-# class LoginForm(forms.ModelForm):
-#     class Meta:
-#         model = UserModel
-#         fields = ('username', 'password')
-#         widgets = {
-#             'username': forms.TextInput(attrs={
-#                 'placeholder': 'Username'
-#             })
-#         }
-
-# def __init__(self, *args, **kwargs):
-#     self.request = kwargs.pop('request', None)
-#     super(LoginForm, self).__init__(*args, **kwargs)
